38030f421277966b47ba4ff53d904ffbafc65a1f
Juan Brizuela <juan.brizuela@michelada.io>
2019-09-27 12:07:21 -0500

    Created new and create actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
