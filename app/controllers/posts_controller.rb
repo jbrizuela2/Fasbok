445f13293483ec0dad275ccecbe3d3ec6193cadb
Juan Brizuela <juan.brizuela@michelada.io>
2019-10-11 11:43:12 -0500

    Added edit, update, destroy actions and devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,21 @@
 class PostsController < ApplicationController
+  before_action :assign_post, only: [:show, :edit, :update, :destroy]
+
+  # Listar todos los elementos
   def index
     @posts = Post.all
   end
 
+  # Ver un solo elemento
   def show
-    @post = Post.find(params[:id])
   end
 
+  # Nuevo Elemento
   def new
     @post = Post.new
   end
 
+  # Crear elemento
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -20,9 +25,35 @@ class PostsController < ApplicationController
     end
   end
 
+  # Editar
+  def edit
+  end
+
+  # Update
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  # Destroy
+  def destroy
+    if @post.destroy
+      redirect_to posts_path
+    else
+      p "Sabes que compa, no se pudo borrar. F"
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def assign_post
+    @post = Post.find(params[:id])
   end
 end
