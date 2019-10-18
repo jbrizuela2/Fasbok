fccf2f5472ce654a934ce9c1569b4082d0101b4b
Juan Brizuela <juan.brizuela@michelada.io>
2019-10-18 12:12:13 -0500

    Created model test for posts

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "model is invalid without required fields" do
+    @post = Post.new
+
+    assert_not @post.valid?
+  end
+
+  test "model is valid with the required fields" do
+    @post = Post.new(title: "test", content: "test", user: users(:juan))
+
+    assert @post.valid?
+  end
+
+  test "it works the other way" do
+    @user = users(:juan)
+
+    @post = @user.posts.new(title: "test", content: "test")
+
+    assert @post.valid?
+  end
+
+  test "user can have many posts" do
+    @user = users(:juan)
+    @post = posts(:one)
+
+    assert_difference "@user.posts.count", 1 do
+      @user.posts.create(title: "test", content: "test")
+    end
+
+    assert_equal 2, @user.posts.count
+  end
 end
